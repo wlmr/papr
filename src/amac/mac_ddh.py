e25b2ce90804cacda993d55029b3d2ab8b0321ff
Wilmer Nilsson <wlmr@protonmail.com>
2021-02-11 15:26:54 +0000

    uses dictionaries for referencing the variables

--- src/amac/mac_ddh.py
+++ src/amac/mac_ddh.py
@@ -14,8 +14,10 @@ def keygen(params, n):
     """ mac DDH keygen """
     assert n > 0
     (G, p, g, h) = params
-    sk = [G.random() for _ in range(2*n+3)] # case n = 1 gives us x0, x1, y0, y1, z (3+2*n).  
-    iparams = [h**ski for ski in sk[:-1]]  
+    #sk = [G.random() for _ in range(2*n+3)] # case n = 1 gives us x0, x1, y0, y1, z (3+2*n).  
+    sk_names = ['x0','x1','y0','y1','z']
+    sk = {name:G.random() for name in sk_names}
+    iparams = {name.upper():h**sk[name] for name in sk_names[:-1]}
     return (sk, iparams)
 
 
@@ -26,15 +28,15 @@ def mac(params, sk, m):
     r = G.random() 
     em = G.encode(m)
     #import pdb; pdb.set_trace()
-    Hx = sk[0] + sk[1] * em   #x0 + x1*m                  # Hx becomes larger than mod value??!??!
-    Hy = sk[2] + sk[3] * em   #y0 + y1*m
+    Hx = sk['x0'] + sk['x1'] * em   #x0 + x1*m                  # Hx becomes larger than mod value??!??!
+    Hy = sk['y0'] + sk['y1'] * em   #y0 + y1*m
     #Hx = sk[0] + sum([sk[i+1] * em[i] for i in range(n)])
     #Hy = sk[0+n] + sum([sk[n+i+1] * em[i] for i in range(n)])
     
     sigma_w = g**r 
     sigma_x = g**(r*Hx)
     sigma_y = g**(r*Hy)
-    sigma_z = g**(r*sk[-1]) # g**(r*z)
+    sigma_z = g**(r*sk['z']) # g**(r*z)
     
     sigma = (sigma_w, sigma_x, sigma_y, sigma_z) 
     return sigma
@@ -46,12 +48,12 @@ def verify(params, sk, m, sigma):
     (G, p, g, h) = params
     (sigma_w, sigma_x, sigma_y, sigma_z) = sigma
     em = G.encode(m)
-    Hx = sk[0] + sk[1]*em   #x0 + x1*m
-    Hy = sk[2] + sk[3]*em   #y0 + y1*m
+    Hx = sk['x0'] + sk['x1']*em   #x0 + x1*m
+    Hy = sk['y0'] + sk['y1']*em   #y0 + y1*m
     #Hx = sk[0]   + sum([sk[i+1]*m[i] for i in range(n)])
     #Hy = sk[0+n] + sum([sk[n+i+1]*m[i] for i in range(n)])
 
-    return sigma_w != 1 and sigma_x == sigma_w**Hx and sigma_y == sigma_w**Hy and sigma_z == sigma_w**sk[-1]
+    return sigma_w != 1 and sigma_x == sigma_w**Hx and sigma_y == sigma_w**Hy and sigma_z == sigma_w**sk['z']
 
 
 #Debug
