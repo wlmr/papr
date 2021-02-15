53af4c67e7c7a4660e3cb46f3488155425c3edcf
Wilmer Nilsson <wlmr@protonmail.com>
2021-02-15 20:01:15 +0000

    adds parts to issuance

--- src/amac/amac.py
+++ src/amac/amac.py
@@ -1,8 +1,11 @@
 from charm.toolbox.integergroup import IntegerGroup
+#from charm.schemes.pkenc.pkenc_elgamal85 import *
 
 from mac_ddh import setup as setup_ddh
 from mac_ddh import keygen as keygen
 from mac_ddh import mac as mac
+
+from elgamal import *
 
 def setup(k):
     return setup_ddh(k)
@@ -10,40 +13,55 @@ def setup(k):
 def cred_keygen(params,n):
     (G,p,g,h) = params
     (sk, iparams) = keygen(params, n)
-    (sk['x_tilde'], sk['y_tilde', sk['z_tilde']) = (G.random(), G.random(), G.random())
+    sk['x_tilde'], sk['y_tilde'], sk['z_tilde'] = G.random(), G.random(), G.random()
     iparams['c_x_0'] = (g**sk['x0']) * (h**sk['x_tilde'])
     iparams['c_y_0'] = (g**sk['y0']) * (h**sk['y_tilde'])
     iparams['c_z']   = (g**sk['z'])  * (h**sk['z_tilde'])
     return (sk,iparams)
 
-def blind_issue(sk, S):
+def blind_issue(params, sk, S):
     pass
 
-def blind_obtain(iparams, m):
-    pass
-
-def show(params, iparams, cred, m, phi):
+def blind_obtain(params, iparams, m):
     (G,p,g,h) = params
-    (C_x_o, X) = iparams
-    r = G.random()
-    z = G.random()
+    el = Elgamal(params)
+    (pk,sk) = el.keygen()
+    d = sk['x']
+    gamma = pk['h']
+    M = g ** (G.encode(m))
+    (e,r) = el.encrypt(pk, M)
+    # send e to issuer along with a proof of knowledge of r,m
 
-    ## z is a vector, X is a vector. But since we only have one message we only need one z (and one X?)
 
 
 
-    (u, u_prime) = cred
-    C_m = u**m * h**z
-    C_u_prime = u_prime * g**r
-    sigma = (u, C_m, C_u_prime)
-    
-    # V is here calulated according to GGM, but with -r instead as in DDH. Is it correct for DDH
-    V = g**(-r) * X**z 
-    ## FIXME: ??
-    #c = hash(params + C_m + C_u_prime + )
 
-    #z = [G.random for x in range(len(m))] # Not necessary since len(m) ==1
+#def show(params, iparams, cred, m, phi):
+#    (G,p,g,h) = params
+#    (C_x_o, X) = iparams
+#    r = G.random()
+#    z = G.random()
+#
+#    ## z is a vector, X is a vector. But since we only have one message we only need one z (and one X?)
+#
+#
+#
+#    (u, u_prime) = cred
+#    C_m = u**m * h**z
+#    C_u_prime = u_prime * g**r
+#    sigma = (u, C_m, C_u_prime)
+#    
+#    # V is here calulated according to GGM, but with -r instead as in DDH. Is it correct for DDH
+#    V = g**(-r) * X**z 
+#    ## FIXME: ??
+#    #c = hash(params + C_m + C_u_prime + )
+#
+#    #z = [G.random for x in range(len(m))] # Not necessary since len(m) ==1
     pass
 
 def show_verify(sk, phi):
     pass
+
+def test():
+    setup(k)
+    blind_issue(1,2)
--- src/amac/elgamal.py
+++ src/amac/elgamal.py
@@ -3,6 +3,13 @@ from charm.toolbox.PKEnc import PKEnc
 
 
 class ElGamal(PKEnc):
+    """
+    Simple Elgamal implementation which lets you import
+    the group along with a generator. Encoding/decoding is
+    done outside of the implementation. Should be instantiated 
+    with ElGamal((G,p,g,h)), where G is a Group of order p, with
+    generators g and h
+    """
 
     def __init__(self, params, p=0, q=0):
         global G
@@ -12,37 +19,31 @@ class ElGamal(PKEnc):
     def keygen(self):
         x = G.random() 
         h = g ** x
-        pk = {'g':g, 'h':h}
+        pk = {'g':g, 'h':h, 'G':G}
         sk = {'x':x}
         return (pk, sk)
 
-    def encrypt(self, pk, M):
-        y = G.random()
+    def encrypt(self, pk, m):
+        y = pk['G'].random()
         c1 = pk['g'] ** y 
         s = pk['h'] ** y
-        c2 = M * s
+        c2 = m * s
         return ({'c1':c1, 'c2':c2}, y)
 
     def decrypt(self, pk, sk, c):
         s = c['c1'] ** sk['x']
         m = c['c2'] * (s ** -1)
-        if G.groupSetting() == 'integer':
-            M = G.decode(m % G.p)
-        elif G.groupSetting() == 'elliptic_curve':
-            M = G.decode(m)
-        return M
+        return m
 
-def test():
+if __name__ == "__main__":
     G = IntegerGroup()
     G.paramgen(100)
     g = G.randomGen()
     h = G.randomGen()
-    m = G.encode(b'god is dead')
+    M = g ** (G.encode(b'god is dead'))
+    print(M)
     params = (G, G.p, g, h)
     el = ElGamal(params)
     (pk, sk) = el.keygen()
-    (c,r) = el.encrypt(pk, m)
-    print(el.decrypt(pk,sk,c))
-
-
-test()
+    (c,r) = el.encrypt(pk, M)
+    print(el.decrypt(pk,sk,c) % G.p)
