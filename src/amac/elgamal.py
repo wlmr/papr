99e7d5d5fb0d5887202a262f62d7cc8459f56649
Wilmer Nilsson <wlmr@protonmail.com>
2021-02-17 18:14:18 +0100

    for Mr. Kron

--- src/amac/elgamal.py
+++ src/amac/elgamal.py
@@ -1,9 +1,7 @@
-#from charm.toolbox.integergroup import IntegerGroup
-from charm.toolbox.PKEnc import PKEnc
-from charm.toolbox.ecgroup import ECGroup, G, ZR, order 
-from charm.toolbox.eccurve import prime192v2
+from petlib.ec import EcGroup
+from petlib.bn import Bn
 
-class ElGamal(PKEnc):
+class ElGamal():
     """
     Simple Elgamal implementation which lets you import
     the group along with a generator. Encoding/decoding is
@@ -13,37 +11,38 @@ class ElGamal(PKEnc):
     """
 
     def __init__(self, params):
-        global group
+        global G
         global g
-        (group,_,g,_) = params
+        global p
+        (G,p,g,_) = params
 
     def keygen(self):
-        x = group.random(ZR) 
-        h = g ** x
-        pk = {'g':g, 'h':h, 'G':group}
+        x = p.random() 
+        h = x * g
+        pk = {'g':g, 'h':h, 'G':G, 'p':p}
         sk = {'x':x}
         return (pk, sk)
 
     def encrypt(self, pk, m):
-        y = pk['G'].random(ZR)
-        c1 = pk['g'] ** y 
-        s = pk['h'] ** y 
-        c2 = (pk['g']**m) * s
+        y = pk['p'].random()
+        c1 = y * pk['g'] 
+        s = y * pk['h'] 
+        c2 = (m * pk['g']) + s
+        print(m * pk['g'] )
         return ({'c1':c1, 'c2':c2}, y)
 
     def decrypt(self, pk, sk, c):
-        s = c['c1'] * sk['x']
-        m = c['c2'] * (s ** -1)
-        return m#c['c2'] - pk['x']*c['c1']
+        return c['c2'] - sk['x'] * c['c1']
 
 if __name__ == "__main__":
-    group = ECGroup(prime192v2)
-    g = group.random(G)
-    h = group.random(G)
-    M = g * group.encode(b'god is dead         ')
-    #print(M)
-    params = (group, group.order(), g, h)
+    G = EcGroup()
+    p = G.order()
+    g = G.generator()
+    h = G.hash_to_point("mac_ggm".encode("utf8"))
+
+    m = Bn.from_binary(b'god is dead')
+    params = (G, p, g, h)
     el = ElGamal(params)
     (pk, sk) = el.keygen()
-    (c,r) = el.encrypt(pk, M)
+    (c,r) = el.encrypt(pk, m)
     print(el.decrypt(pk,sk,c))
