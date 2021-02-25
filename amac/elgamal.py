d14d29930388bf4eab8f7629369424df6d7a3908
Wilmer Nilsson <wlmr@protonmail.com>
2021-02-25 14:34:19 +0100

    elgamal: unclass'd

--- amac/elgamal.py
+++ amac/elgamal.py
@@ -1,48 +1,41 @@
 from petlib.ec import EcGroup
 from petlib.bn import Bn
 
-#TODO: decide whether this class should be exponential elgamal,
-# i.e. m gets raised to g in the algorithm itself. (currently is)
-class ElGamal():
+
+def keygen(params):
     """
-    Simple Elgamal implementation which lets you import
+    Simple exponential Elgamal implementation which lets you import
     the group along with a generator. Encoding/decoding is
-    done outside of the implementation. Should be instantiated 
+    done outside of the implementation. Should be instantiated
     with ElGamal((G,p,g,h)), where G is a Group of order p, with
     generators g and h
     """
+    (g, p) = params
+    x = p.random()
+    h = x * g
+    pk = {'g': g, 'h': h, 'p': p}
+    sk = {'x': x}
+    return (pk, sk)
 
-    def __init__(self, params):
-        global G
-        global g
-        global p
-        (G,p,g,_) = params
 
-    def keygen(self):
-        x = p.random() 
-        h = x * g
-        pk = {'g':g, 'h':h, 'p':p}
-        sk = {'x':x}
-        return (pk, sk)
+def encrypt(pk, m):
+    y = pk['p'].random()
+    c1 = y * pk['g']
+    c2 = m * pk['g'] + y * pk['h']
+    return ({'c1': c1, 'c2': c2}, y)
 
-    def encrypt(self, pk, m):
-        y = pk['p'].random()
-        c1 = y * pk['g'] 
-        c2 = m * g + y * pk['h']
-        return ({'c1':c1, 'c2':c2}, y)
 
-    def decrypt(self, sk, c):
-        return c['c2'] - sk['x'] * c['c1']
+def decrypt(sk, ciphertext):
+    return ciphertext['c2'] - sk['x'] * ciphertext['c1']
+
 
 if __name__ == "__main__":
     G = EcGroup()
     p = G.order()
     g = G.generator()
-    h = G.hash_to_point(b"h")
 
-    m = Bn.from_binary(b'god is dead') * g
-    params = (G, p, g, h)
-    el = ElGamal(params)
-    (pk, sk) = el.keygen()
-    (c,r) = el.encrypt(pk, m)
-    print(el.decrypt(pk,sk,c))
+    m = Bn.from_binary(b'god is dead')
+    params = (g, p)
+    (pk, sk) = keygen()
+    (ciphertext, r) = encrypt(pk, m)
+    print(decrypt(pk, sk, ciphertext))
