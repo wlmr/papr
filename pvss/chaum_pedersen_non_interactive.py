da4d8ebc625dfdc73b3d0a7da3193936c64cfd44
Patrik Kron <17673053+KronP@users.noreply.github.com>
2021-02-25 16:57:43 +0100

    Improve variable names

--- pvss/chaum_pedersen_non_interactive.py
+++ pvss/chaum_pedersen_non_interactive.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python3
 from petlib.ec import EcGroup, EcPt
 from petlib.bn import Bn
-#from dealer import PVSS
 from hashlib import sha256
-
-
-#class DLEQ():#
-
-    # def __init__(self, params):
-    #     global G
-    #     global g
-    #     global p
-    #     global G
-    #     (Gq, p, g, G) = params
-        
 
 
 def get_X_i_list(commitments, n):
--- pvss/dealer.py
+++ pvss/dealer.py
@@ -10,7 +10,7 @@ A = tuple[Bn, Bn]
 #Polynomial = list([Bn])
 
 
-class PVSS_prover():
+class PVSS_issuer():
 
     def __init__(self, params):
         global Gq
@@ -27,12 +27,12 @@ class PVSS_prover():
         assert len(pub_keys) == n
         assert secret == secret % p
 
-        px = self.gen_polynomial(t, secret)
+        px = self.__gen_polynomial(t, secret)
 
-        commitments = self.get_commitments(g, px)
-        shares_list = self.calc_shares(px, t, n)
+        commitments = self.__get_commitments(g, px)
+        shares_list = self.__calc_shares(px, t, n)
 
-        enc_shares = self.get_encrypted_shares(pub_keys, shares_list)
+        enc_shares = self.__get_encrypted_shares(pub_keys, shares_list)
         X_i_list = cpni.get_X_i_list(commitments, n)
 
         pub = {'C_list': commitments, 'Y_list': enc_shares, 'X_list': X_i_list}
@@ -49,25 +49,27 @@ class PVSS_prover():
 
         return (pub, proof)
 
-    def gen_polynomial(self, t, secret):
-        # t-1 constants including secret, thus t-2 random
+    def __gen_polynomial(self, t, secret):
+        '''
+        Generate polynomial
+        '''
         px_rand = [p.random() for i in range(t-1)]
         px = [secret] + px_rand
         return px
 
-    def calc_shares(self, px, t, n):
-        return [self.calc_poly(px, t, i) for i in range(1, n+1)]
+    def __calc_shares(self, px, t, n):
+        return [self.__calc_share(px, t, i) for i in range(1, n+1)]
 
-    def calc_poly(self, px, t, x):
+    def __calc_share(self, px, t, x):
         result = 0
         for (alpha, j) in zip(px, range(t)):
             result = (result + alpha * (x**j)) % p
         return result
 
-    def get_commitments(self, g, px):
+    def __get_commitments(self, g, px):
         return [p_i * g for p_i in px]
 
-    def get_encrypted_shares(self, pub_keys, shares):
+    def __get_encrypted_shares(self, pub_keys, shares):
         assert len(pub_keys) == len(shares)
         # FIXME: Should we have mod p
         Y_i_list = [shares[i]*y_i for (y_i, i)
@@ -75,15 +77,21 @@ class PVSS_prover():
         return Y_i_list
 
     def decode(self, S_list, t):
+        '''
+        Calulates secret from participants decrypted shares
+        '''
         assert len(S_list) == t
-        ans = self.lagrange(1, t) * S_list[0]
+        ans = self.__lagrange(1, t) * S_list[0]
 
         for (S_i, i) in zip(S_list[1:], range(2, t+1)):
-            ans = ans + self.lagrange(i, t) * S_i
+            ans = ans + self.__lagrange(i, t) * S_i
 
         return ans  # G**s
 
-    def lagrange(self, i, t) -> int:
+    def __lagrange(self, i, t) -> int:
+        '''
+        Calculate lagrange coefficient
+        '''
         res = 1
         for j in range(1, t+1):
             if j != i:
@@ -93,7 +101,9 @@ class PVSS_prover():
 
 
     def verify_correct_decryption(self, S_i, Y_i, decrypt_proof, pub_key):
-        #import pdb; pdb.set_trace()
+        '''
+        Verify that a participants proof of correct decryption of their share
+        '''
         (c_claimed, r, a_1, a_2) = decrypt_proof
         y_i = pub_key
 
@@ -108,6 +118,9 @@ class PVSS_prover():
         return False
 
     def batch_verify_correct_decryption(self, proved_decryptions, Y_list, pub_keys):
+        '''
+        Verify all paricipants decryption of shares
+        '''
         for ((S_i, decrypt_proof), Y_i, pub_key) in zip(proved_decryptions, Y_list, pub_keys):
             if self.verify_correct_decryption(S_i, Y_i, decrypt_proof, pub_key) == False:
                 return False
@@ -117,7 +130,7 @@ class PVSS_prover():
 
 
 
-class PVSS_verifyer():
+class PVSS_participant():
 
     def __init__(self, params):
         global Gq
@@ -156,18 +169,18 @@ if __name__ == "__main__":
 
     params = (Gq, p, g, G)
 #    cp = DLEQ(params)
-    pvss_prover = PVSS_prover(params)
+    issuer = PVSS_issuer(params)
 
     n = 4
     t = 3
 
     #demo_priv_keys = [p.random() for i in range(n)]
-    participants = [PVSS_verifyer(params) for i in range(n)]
-    pub_keys = [p.generate_key_pair() for p in participants]
+    participants = [PVSS_participant(params) for i in range(n)]
+    pub_keys = [participant.generate_key_pair() for participant in participants]
 
     #demo_pub_keys = [priv_key * G for priv_key in demo_priv_keys]
 
-    (pub, proof) = pvss_prover.gen_proof(t, n, m, pub_keys)
+    (pub, proof) = issuer.gen_proof(t, n, m, pub_keys)
 
     #proof = pvss.DLEQ_prove_list(pub, demo_pub_keys, shares_list)
 
@@ -183,14 +196,14 @@ if __name__ == "__main__":
     #    private_key, enc_share) in zip(demo_priv_keys, pub['Y_list'])]
 
 
-    proved_decryptions = [p.participant_decrypt_and_prove(enc_share) for (p, enc_share) in zip(participants, pub['Y_list'])]
+    proved_decryptions = [participant.participant_decrypt_and_prove(enc_share) for (participant, enc_share) in zip(participants, pub['Y_list'])]
 
-    if pvss_prover.batch_verify_correct_decryption(proved_decryptions, pub['Y_list'], pub_keys) == False:
+    if issuer.batch_verify_correct_decryption(proved_decryptions, pub['Y_list'], pub_keys) == False:
         print("Verification of decryption failed")
 
     S_list = [S_i for (S_i, decrypt_proof) in proved_decryptions]
 
-    actual_decryption = pvss_prover.decode(S_list[0:-1], t)
+    actual_decryption = issuer.decode(S_list[0:-1], t)
 
     print("Test decrypt")
     assert expected_decryption == actual_decryption
