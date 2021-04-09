810f30fe31bf0ad925635fd8df12e2a67618fd66
Patrik Kron <17673053+KronP@users.noreply.github.com>
2021-04-09 08:23:24 +0000

    Add method and test converting pubkey to btc_addr

--- papr/utils.py
+++ papr/utils.py
@@ -1,8 +1,16 @@
 from hashlib import sha256
 from petlib.bn import Bn
 from petlib.pack import encode
+import bit
+from petlib.ec import EcPt
 
 
 def hash(elements: list) -> Bn:
     bin_hash = sha256(encode(elements)).digest()
     return Bn.from_binary(bin_hash)
+
+
+def pub_key_to_addr(pub_key):
+    if isinstance(pub_key, EcPt):
+        pub_key = pub_key.export()
+    return bit.format.public_key_to_address(pub_key, version="test")
--- test/test_papr_split_btc.py
+++ test/test_papr_split_btc.py
@@ -6,6 +6,7 @@ from papr.papr_issuer import Issuer
 from amac.credential_scheme import setup as setup_cmz
 from bit.format import bytes_to_wif
 from petlib.ec import EcPt
+import papr.utils as utils
 
 
 class TestBTC:
@@ -45,3 +46,52 @@ class TestBTC:
         public_key_in_ecpt_format = EcPt.from_binary(key2.public_key, G)
 
         assert public_key_in_ecpt_format == this_should_be_btc_public_key
+
+    def test_if_pubkey_is_same(self):
+        issuer = Issuer()
+        (k, n) = (3, 10)
+        (y_sign, y_encr), iparams, _, _, _, _ = issuer.setup(k, n)
+
+        params = issuer.get_params()
+        (G, _, g0, _) = params
+
+        btc_key = PrivateKeyTestnet()
+
+        # Convert private key to Bn format
+        btc_priv_key = Bn.from_decimal(str(btc_key.to_int()))
+
+        user = User(issuer.get_params(), iparams, y_sign, y_encr, k, n, btc_priv_key)
+
+        (_, this_should_be_btc_public_key) = user.req_cred_sign()
+
+        # Convert to uncompressed format:
+        wif = bytes_to_wif(btc_key.to_bytes(), compressed=False)
+        key2 = PrivateKeyTestnet(wif)
+
+        public_key_in_ecpt_format = EcPt.from_binary(key2.public_key, G)
+        assert EcPt.from_binary(key2.public_key, G) == EcPt.from_binary(btc_key.public_key, G)
+        # assert key2.public_key == btc_key.public_key # These are different, but somehow the import to EcPt makes it the same?
+
+        assert public_key_in_ecpt_format == this_should_be_btc_public_key
+
+    def test_pubkey_to_addr(self):
+        issuer = Issuer()
+        (k, n) = (3, 10)
+        (y_sign, y_encr), iparams, _, _, _, _ = issuer.setup(k, n)
+
+        params = issuer.get_params()
+        (G, _, g0, _) = params
+
+        btc_key = PrivateKeyTestnet()
+
+        # Convert private key to Bn format
+        btc_priv_key = Bn.from_decimal(str(btc_key.to_int()))
+        user = User(issuer.get_params(), iparams, y_sign, y_encr, k, n, btc_priv_key)
+        (_, this_should_be_btc_public_key) = user.req_cred_sign()
+
+        adr = btc_key.address
+
+        new_adr = utils.pub_key_to_addr(this_should_be_btc_public_key.export())
+
+        assert adr == new_adr
+        assert utils.pub_key_to_addr(this_should_be_btc_public_key) == adr
