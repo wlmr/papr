df89b6291b0ce85db2f60f378935da0064e14a85
Wilmer Nilsson <wlmr@protonmail.com>
2021-04-23 19:10:47 +0200

    simulation init

---
+++ simulation/sim.py
@@ -0,0 +1,13 @@
+from papr_money.customer_with_issuer import Customer
+from papr_money.vendor import Vendor
+
+# def run():
+#     vendor = Vendor()
+#     customer = Customer("Moxie", vendor)
+
+
+# def customer_enroll(customer):
+#     customer.req_enroll()
+
+# def customer_login(customer):
+#     customer.
