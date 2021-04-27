982e0b3a6ecb0d6be63338ab3a74549d65877953
Patrik Kron <17673053+KronP@users.noreply.github.com>
2021-04-27 16:28:08 +0000

    Reenable tests

--- test/test_papr_money.py
+++ test/test_papr_money.py
@@ -5,19 +5,19 @@ import pytest
 from papr.utils import pub_key_to_addr
 from bit import PrivateKeyTestnet
 from test.procedures import bootstrap_procedure, enroll_procedure, authentication_procedure, revoke_procedure
-# from bit.network import NetworkAPI
+from bit.network import NetworkAPI
 
 
 class TestPaprMoney:
 
-    # @pytest.fixture(autouse=True)
-    # def run_before_and_after_tests(tmpdir):
-    #     """Runs code befora and after each test"""
-    #     # Setup
-    #     node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
+    @pytest.fixture(autouse=True)
+    def run_before_and_after_tests(tmpdir):
+        """Runs code befora and after each test"""
+        # Setup
+        node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
 
-    #     yield  # Run a test
-    #     # Teardown 
+        yield  # Run a test
+        # Teardown 
 
 
     def test_registry(self):
@@ -42,17 +42,17 @@ class TestPaprMoney:
 
     def test_transaction_to_unregistered_user(self):
         not_registered_pub_addr = PrivateKeyTestnet().address
-        # node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
+        node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
 
 
-        # node.importaddress(not_registered_pub_addr, "not_reg", True)
+        node.importaddress(not_registered_pub_addr, "not_reg", True)
 
         k, n = 2, 3
         bank = Bank()
         params, (y_sign, y_encr), iparams, _, _, _, _ = bank.setup(2, 3)
         customer = Customer("Josip Tito", bank, params, iparams, y_sign, y_encr, k, n)
       
-        # node.importaddress(customer.get_address(), "test_addr", True)
+        node.importaddress(customer.get_address(), "test_addr", True)
 
         assert float(customer.get_balance("satoshi")) > 0.0
         assert customer.send(not_registered_pub_addr, 1, 'satoshi', bank) is None
@@ -71,14 +71,14 @@ class TestPaprMoney:
     # @pytest.mark.skip(reason="Disable since github actions creates a new wallet every run. Therefore the wallet will always be empty.")
     # NOTE: give Tito more coins if this test fails
     def test_customer_balance(self):
-        # node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
+        node = NetworkAPI.connect_to_node(user='admin1', password='123', host='localhost', port='19001', use_https=False, testnet=True)
         
         k, n = 3, 10
         bank = Bank()
         params, (y_sign, y_encr), iparams, _, _, _, _ = bank.setup(3, 10)
         customer = Customer("Josip Tito", bank, params, iparams, y_sign, y_encr, k, n)
 
-        # node.importaddress(customer.get_address(), "test_addr", True)
+        node.importaddress(customer.get_address(), "test_addr", True)
 
         assert float(customer.get_balance("satoshi")) > 0.0
 
