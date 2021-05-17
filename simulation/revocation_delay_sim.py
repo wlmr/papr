a4614826ad64722dcf5fa9bedfb115672a806e69
Wilmer Nilsson <wlmr@protonmail.com>
2021-05-17 18:24:43 +0200

    multicore delay sim

--- simulation/revocation_delay_sim.py
+++ simulation/revocation_delay_sim.py
@@ -8,22 +8,22 @@ from simulation.procedures import bootstrap_procedure
 from queue import PriorityQueue
 from dataclasses import dataclass, field
 from typing import Any
+from multiprocessing import Pool
 
 
-nbr_of_customers = 20
-seconds_per_day = 10
+nbr_of_customers = 250
+seconds_per_day = 20
 mu = 2 * seconds_per_day
 sigma = 1 * seconds_per_day
-login_interval = [gauss(mu, sigma) for _ in range(nbr_of_customers)]
+login_interval = [abs(gauss(mu, sigma)) for _ in range(nbr_of_customers)]
 revocation_timer = {}
-d_time_logins = 5 * seconds_per_day  # days
 d_time_revokations = 1 * seconds_per_day  # day
 rev_counter = 0
 bank = Bank()
 customers = []
 customer_queue = PriorityQueue()
 start_time = time.perf_counter()
-k, n = 4, 5
+k, n = 20, 200
 
 
 @dataclass(order=True)
@@ -32,12 +32,25 @@ class PrioritizedCustomer:
     customer: Any = field(compare=False)
 
 
-def run():
+def run(_mu, _sigma, _k, _n):
+    global mu
+    global sigma
+    global k
+    global n
     global customers
+    global login_interval
+    mu = _mu * seconds_per_day
+    sigma = _sigma * seconds_per_day
+    k = _k
+    n = _n
+    login_interval = [abs(gauss(mu, sigma)) for _ in range(nbr_of_customers)]
     customers = bootstrap_procedure(k, n, bank, login_interval)
     customers = customers + [Customer(f"customer{i}", bank, login_interval[i]) for i in range(n+1, nbr_of_customers)]
     for i in range(nbr_of_customers):
-        customer_queue.put(PrioritizedCustomer(time.perf_counter(), customers[i]))
+        if not customers[i].is_enrolled:
+            customers[i].req_enroll()
+            customers[i].req_cred()
+        customer_queue.put(PrioritizedCustomer(time.perf_counter()+login_interval[i], customers[i]))
     with ThreadPoolExecutor(max_workers=2) as executor:
         executor.submit(customer_thread_run)
         executor.submit(run_bank_thread)
@@ -78,7 +91,6 @@ def run_customer(customer):
         else:
             customer.curl_rev_list()
             address = choice(list(customer.issuer.registry.values()))
-            # txnid = customer.send(address, 1, "satoshi")
             logging.info(f"{customer.name} sent 1 satoshi to {address}")
     return False
 
@@ -88,7 +100,6 @@ def run_bank_thread():
     rev_complete_counter = 0
     revoked = set()
     revoking = set()
-    time.sleep(d_time_logins)
     global revocation_timer
     no_more_left_to_revoke = False
     while not no_more_left_to_revoke:
@@ -98,8 +109,11 @@ def run_bank_thread():
                 no_more_left_to_revoke = True
                 break
             rev_pub_cred = choice(bank.cred_list.read())
+        if no_more_left_to_revoke:
+            break
         revocation_timer[rev_pub_cred] = [time.perf_counter()]
         logging.info("The Bank is revoking a public credential!")
+        print("The Bank is revoking a public credential!")
         rev_request_counter += 1
         revoking.add(rev_pub_cred)
         bank.get_rev_data(rev_pub_cred)
@@ -114,6 +128,8 @@ def run_bank_thread():
                     for identity, pub_id in bank.user_list.read():
                         if rev_pub_id == pub_id:
                             logging.info(
+                                f"""Bank revoked {identity}. {rev_complete_counter} out of {rev_request_counter} requests has been successfully revoked.""")
+                            print(
                                 f"""Bank revoked {identity}. {rev_complete_counter} out of {rev_request_counter} requests has been successfully revoked.""")
         time.sleep(d_time_revokations)
 
@@ -138,38 +154,45 @@ def print_revocation_times():
     """
     Prints the delta revocation time, for each request.
     """
-    with open(f"{k}-{n}-{nbr_of_customers}-revocation-times.log", "w") as file:
-        file.write("BEGIN\n")
-        [file.write(f"{t[0]}: revocation request nbr {i+1}! Restored after {t[1]-t[0]} seconds, ({(t[1]-t[0])/seconds_per_day} days)\n")
+    with open(f"{k}-{n}-{nbr_of_customers}-{mu}-{sigma}-revocation-times.log", "w") as file:
+        file.write("request_number;was_restored;delay\n")
+        [file.write(f"{i+1};True;{(t[1]-t[0])/seconds_per_day}\n")
          if len(t) == 2
-         else file.write(f"{t[0]}: revocation request nbr {i+1}! Was never restored. Time since request: {time.perf_counter()-t[0]} seconds, ({(time.perf_counter()-t[0])/seconds_per_day} days)\n")
+         else file.write(f"{i+1};False;{(time.perf_counter()-t[0])/seconds_per_day}\n")
          for (k, t), i in zip(revocation_timer.items(), range(len(revocation_timer)))]
-        file.write("END\n")
 
 
 def print_nbr_logins():
     """
     Just to check that the queue is working, and that no one gets left out.
     """
-    with open(f"{k}-{n}-{nbr_of_customers}-nbr-of-logins_per_user.log", "w") as file:
+    with open(f"{k}-{n}-{nbr_of_customers}-{mu}-{sigma}-nbr-of-logins_per_user.log", "w") as file:
         file.write("BEGIN\n")
         [file.write(f"{c.name}: {c.nbr_logins}, (login interval: {c.login_interval/seconds_per_day} day(s))\n")
          for c in customers]
         file.write("END\n")
 
 
-def main():
-    logging.basicConfig(format='%(asctime)s %(message)s', filename=f'revocation_delay_{k}_{n}_sim.log', level=logging.INFO)
-    logging.info('Started')
-    run()
-    logging.info('Finished')
-
-
-if __name__ == '__main__':
+def main(params):
     try:
-        main()
+        mu, sigma, k, n = params
+        logging.basicConfig(format='%(asctime)s %(message)s', filename=f'revocation_delay_{k}_{n}_{nbr_of_customers}-{mu}-{sigma}-sim.log', level=logging.INFO)
+        logging.info('Started')
+        run(mu, sigma, k, n)
+        logging.info('Finished')
     except KeyboardInterrupt:
         pass
     finally:
         print_revocation_times()
         print_nbr_logins()
+
+
+if __name__ == '__main__':
+    params = []
+    for mu, sigma in [(2, 0.5), (7, 2)]:
+        for k in [5, 20]:
+            for n in [50, 100, 150, 200]:
+                params.append((mu, sigma, k, n))
+    with Pool() as p:
+        p.map(main, params)
+    
